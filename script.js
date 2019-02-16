5badfb4406910f05100e9c5197e702d5ea383d4f
CodeProjects123 <46411900+CodeProjects123@users.noreply.github.com>
2019-02-16 16:06:27 -0600

    added code to allow button functionality

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //allows for the button in index to be pressed and taken to another page
 function doSomething(){
-    window.open("personalLife.html");
+    // variables that access the radio button id 
+   var radioButton1 = document.getElementById("radio1").checked;
+   var radioButton2 = document.getElementById("radio2").checked;
+   var radioButton3 = document.getElementById("radio3").checked;
+   //checks conditions
+    if (radioButton1){
+        //opens pg
+        window.open("personalLife.html");
+    }
+    else if(radioButton2){
+        window.open("work.html");
+    }
+    else if(radioButton3){
+        window.open("school.js");
+    }
+    
 }
