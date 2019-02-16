385ba24246ba231a182eba2d43707f3f5711d1f6
CodeProjects123 <46411900+CodeProjects123@users.noreply.github.com>
2019-02-15 22:20:46 -0600

    changed the link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //allows for the button in index to be pressed and taken to another page
 function doSomething(){
-    window.open("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_button_css");
+    window.open("personalLife.html");
 }
